9388004921d17306ff7f62b92e2c8e4ebfd377ea
Pranay Bhawsar <pranay.bhawsar@gmail.com>
2015-04-29 14:27:11 -0700

    Create ReverseRecursive.java

---
+++ SimpleProgram/src/ReverseRecursive.java
@@ -0,0 +1,22 @@
+
+public class ReverseRecursive {
+
+	public  static String recReverse(String str){
+		
+		if (str.length()<2) 
+			return str;
+		else{
+			return recReverse(str.substring(1))+ str.charAt(0);
+		}
+	}
+	
+	public static void main(String [] args){
+		
+		  System.out.println(recReverse("hello"));
+		  System.out.println(recReverse("premchand"));
+		  System.out.println(recReverse("pranay"));
+		  System.out.println(recReverse("ReverseMe"));
+		
+	}
+	
+}
